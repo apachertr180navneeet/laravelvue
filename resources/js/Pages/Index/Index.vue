b33ba857f598e6e307f59f51f03fd17853c04a6d
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2023-03-30 22:37:25 +0530

    default layput

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -8,21 +8,10 @@
         The message is {{ messageFrom }}
     </div>
 </template>
-
-
-
 <script setup>
     import {Link} from '@inertiajs/vue3'
     defineProps({
         // veriable name from controller
         messageFrom : String
     })
-</script>
-
-
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue';
-    export default {
-        layout: MainLayout
-    }
 </script>
