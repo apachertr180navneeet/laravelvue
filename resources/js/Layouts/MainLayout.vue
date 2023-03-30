590a4ab00ca9dd6403446a87fa02614a06142346
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2023-03-30 21:33:24 +0530

    rictivity in vue

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,9 @@
 <template>
         <Link href="/">Main Page</Link>&nbsp;
         <Link href="/hello">Show Page</Link>
+        <div>
+            The Page with time {{ timer }}
+        </div>
         <slot>
 
         </slot>
@@ -11,5 +14,10 @@
 <script setup>
 
     import {Link} from '@inertiajs/vue3'
+    import {ref} from 'vue';
+
+    const timer = ref(0) //value
+
+    setInterval(() => timer.value++, 1000)
 
 </script>
