742528980a5128dc43a51b8e4375dde6e29bf4d1
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2023-04-01 20:39:14 +0530

    laravel query

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index(){
+        //dd(Listing::all());
         // return vue file base path is all ways resource/js/pages
         // always use variable camel case
         return Inertia(
