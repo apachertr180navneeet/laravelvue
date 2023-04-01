f03d3172bb2fa73ac54a6b9ec073aaf310dfa129
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2023-04-01 21:03:14 +0530

    insert and update and timestamp

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,6 +11,12 @@ class IndexController extends Controller
         //dd(Listing::all());
         // return vue file base path is all ways resource/js/pages
         // always use variable camel case
+
+        // $listing = Listing::create([
+        //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
+        // ]);
+
+        // dd($listing);
         return Inertia(
             'Index/Index',
             [
